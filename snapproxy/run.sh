787877b3602b32aece30f47047cbbf2dd13e3448
Mike Abrahall <mike@abrahall.id.au>
2022-08-02 16:19:03 +1000

    Temporary debugging changes

--- snapproxy/run.sh
+++ snapproxy/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bashio
 
-mkdir -p /share/snapfifo
-mkdir -p /share/snapcast
+set -eEu -o pipefail
+shopt -s failglob
+trap 'bashio::log.info "${BASH_SOURCE:-$0}:${LINENO}: unknown error"' ERR
 
 config=/etc/snapserver.conf
 
@@ -11,15 +12,26 @@ if ! bashio::fs.file_exists '/etc/snapserver.conf'; then
 fi
 bashio::log.info "Populating snapserver.conf..."
 
+bashio::log.info "WTF!? $(ls /)"
+bashio::log.info "WTF!? $(cat /data/options.json)"
+
 # Start creation of configuration
 
+bashio::log.info "What is all of the config? $(bashio::api.supervisor GET '/addons/self/options/config' false)"
+bashio::log.info "Or maybe it's just the first time we read the config $(bashio::config 'stream')"
 echo "[stream]" > "${config}"
-for stream in $(bashio::config 'stream.streams'|sed "s/\$UPSTREAM_SNAPSERVER/$(bashio::config 'upstream_snapserver')/g"); do
-    echo "stream = ${stream}" >> "${config}"
+bashio::log.info "looping"
+for stream in $(bashio::config 'stream.sources') ; do # |sed "s/\$UPSTREAM_SNAPSERVER/$(bashio::config 'upstream_snapserver')/g"); do
+    bashio::log.info "stream... $stream"
+    echo "source = ${stream}" >> "${config}"
 done
+bashio::log.info "buffer... $(bashio::config 'stream.buffer')"
 echo "buffer = $(bashio::config 'stream.buffer')" >> "${config}"
+bashio::log.info "codec..."
 echo "codec = $(bashio::config 'stream.codec')" >> "${config}"
+bashio::log.info "send_to_muted..."
 echo "send_to_muted = $(bashio::config 'stream.send_to_muted')" >> "${config}"
+bashio::log.info "sampleformat..."
 echo "sampleformat = $(bashio::config 'stream.sampleformat')" >> "${config}"
 
 echo "[http]" >> "${config}"
@@ -36,7 +48,9 @@ echo "[server]" >> "${config}"
 echo "threads = $(bashio::config 'server.threads')" >> "${config}"
 
 echo "[server]" >> "${config}"
-echo "datadir = $(bashio::config 'server.datadir')" >> "${config}"
+datadir=$(bashio::config 'server.datadir')
+mkdir -p "$datadir"
+echo "datadir = $datadir" >> "${config}"
 
 bashio::log.info "Starting SnapServer..."
 
