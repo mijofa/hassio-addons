035a36c47f337857f21dfc63195c1e7620ae9b13
Mike Abrahall <mike@abrahall.id.au>
2022-08-20 00:58:01 +1000

    Fix up SSL config options

--- matrixdotorg-synapse/start-optionsjson.py
+++ matrixdotorg-synapse/start-optionsjson.py
@@ -17,10 +17,34 @@ if not OPTIONS_FILE.exists():
 #        If so, this dict will be in an unpredictable state going back into the next main call
 env_opts = json.loads(OPTIONS_FILE.read_text())
 
+# Create symlinks for SSL certs and such
+symlink_paths = {
+    'tls_certificate_path': '/data/{SYNAPSE_SERVER_NAME}.tls.crt'.format(**env_opts),
+    'tls_private_key_path': '/data/{SYNAPSE_SERVER_NAME}.tls.key'.format(**env_opts),
+    'signing_key_path': '/data/{SYNAPSE_SERVER_NAME}.signing.key'.format(**env_opts),
+}
+for key in symlink_paths.keys():
+    symlink_path = pathlib.Path(symlink_paths[key])
+    symlink_dest = pathlib.Path(env_opts.pop(key).format(**env_opts))
+    if symlink_path.exists():
+        if symlink_path.readlink().resolve() != symlink_dest.resolve():
+            raise Exception(f"Symlink '{symlink_path}' already exists and doesn't match")
+        else:
+            print('Skipping symlink', symlink_path, '->', symlink_dest)
+    else:
+        print('Symlinking', symlink_path, '->', symlink_dest)
+        symlink_path.symlink_to(symlink_dest)
+
 # Turn all non-str options into strings.
 # Dropping that are false bools as we actually don't want those set at all.
 for key in list(env_opts.keys()):
-    if env_opts[key] is False:
+    if key == 'SYNAPSE_REPORT_STATS':
+        # This one's special because it's required but can be "no"
+        if env_opts[key] is False:
+            env_opts[key] = "no"
+        else:
+            env_opts[key] = "yes"
+    elif env_opts[key] is False:
         env_opts.pop(key)
     elif env_opts[key] is True:
         env_opts[key] = "true"
