390fc071467c49f1a566eefac42fd95adb0dadf3
Mike Abrahall <mike@abrahall.id.au>
2022-08-23 21:54:59 +1000

    Deal with registration.yaml on first run

--- mautrix-facebook/start-optionsjson.py
+++ mautrix-facebook/start-optionsjson.py
@@ -5,31 +5,48 @@ import json
 import pathlib
 # import secrets
 import subprocess
+import sys
 
 import yaml
 
 OPTIONS_FILE = pathlib.Path('/data/options.json')
 CONFIG_FILE = pathlib.Path('/data/config.yaml')
+REGISTRATION_FILE = pathlib.Path('/data/registration.yaml')
+REGISTRATIONS_DIR = pathlib.Path('/share/matrix_appservices')
 
 if not OPTIONS_FILE.exists():
     raise Exception("No /data/options.json file")
 
 HA_options = json.loads(OPTIONS_FILE.read_text())
 
-# internal_options = {}
-# for k in ['internally', 'used', 'keys', 'which', 'synapse', 'should not', 'see']:
-#     internal_options[k] = HA_options.pop(k)
+# Get the as & hs tokens directly from the registation yaml file used by Synapse
+if REGISTRATION_FILE.exists():
+    registration_conf = yaml.safe_load(REGISTRATION_FILE.read_text())
+    HA_options['as_token'] = registration_conf['as_token']
+    HA_options['hs_token'] = registration_conf['hs_token']
+else:
+    HA_options['as_token'] = "This value is generated when generating the registration"
+    HA_options['hs_token'] = "This value is generated when generating the registration"
 
-# # FIXME: This will generate a new secret every single time.
-# #        Perhaps we could leave it in a file next to homeserver.yaml for referencing next time.
-# for secret in ['as_token:', 'hs_token:']:
-#     if not HA_options.get(secret):
-#         HA_options[secret] = secrets.token_urlsafe()
-
-# Reading this in as YAML to dump it back out is unnecessary, but does't hurt, and might make YAML syntax errors more obvious
+# Reading this in as YAML to dump it back out is unnecessary, but doesn't hurt, and might make YAML syntax errors more obvious
 application_conf = yaml.safe_load(HA_options['config.yaml'].format(**HA_options))
 
 if __name__ == "__main__":
     print("Overwriting config.yaml with custom config", flush=True)
     CONFIG_FILE.write_text(yaml.dump(application_conf))
-    subprocess.check_call(['/opt/mautrix-facebook/docker-run.sh'])
+    if not REGISTRATION_FILE.exists():
+        # Appservice registration file has not been created, this is probably the first run.
+        # So let's generate it and copy it somewhere Synapse can find it.
+        subprocess.check_call(['/opt/mautrix-facebook/docker-run.sh'])
+        assert REGISTRATION_FILE.exists()
+        if not REGISTRATIONS_DIR.is_dir():
+            REGISTRATIONS_DIR.mkdir()
+
+        # FIXME: Should I be regularly updating this?
+        (REGISTRATIONS_DIR / 'mautrix-facebook.yaml').write_text(REGISTRATION_FILE.read_text())
+        print("Appservice registration yaml generated, go sort out registration before restarting this addon.",
+              file=sys.stderr, flush=True)
+    else:
+        # Start normally
+        print("Starting service", flush=True)
+        subprocess.check_call(['/opt/mautrix-facebook/docker-run.sh'])
