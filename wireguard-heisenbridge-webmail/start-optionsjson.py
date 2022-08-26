f6064e294f479d2dd9d7f337ceefbb184148a5c7
Mike Abrahall <mike@abrahall.id.au>
2022-08-26 22:06:18 +1000

    Strip roundcube out and use base Debian Home Assistant image

--- wireguard-heisenbridge-webmail/start-optionsjson.py
+++ wireguard-heisenbridge-webmail/start-optionsjson.py
@@ -4,35 +4,15 @@ import json
 import os
 import pathlib
 # import secrets
+import socket
 import subprocess
 import sys
-import urllib.parse
-import urllib.request
 
 import yaml
 
 OPTIONS_FILE = pathlib.Path('/data/options.json')
 WIREGUARD_CONF = pathlib.Path('/etc/wireguard/wg0.conf')
-REGISTRATION_FILE = pathlib.Path('/share/matrix_appservices/roundcube_heisenbridge_wireguard.yaml')
-
-# I've symlinked some Roundcube directories into /data, but now Roundcube expects them to exist first
-# FIXME: Is it safe to just symlink them to /data directly?
-CONFIG_DIR = pathlib.Path('/data/config')
-DB_DIR = pathlib.Path('/data/db')
-
-
-def parse_dburl_for_roundcube(url: str):
-    """Parse URL string into a set of env vars for Roundcube."""
-    # FIXME: This could probably be a generator
-    parsed_url = urllib.parse.urlparse(url)
-    return {
-        'ROUNDCUBEMAIL_DB_TYPE': 'pgsql' if parsed_url.scheme == 'postgres' else parsed_url.scheme if parsed_url.scheme else None,
-        'ROUNDCUBEMAIL_DB_HOST': parsed_url.hostname if parsed_url.hostname else None,
-        'ROUNDCUBEMAIL_DB_PORT': parsed_url.port if parsed_url.port else None,
-        'ROUNDCUBEMAIL_DB_USER': parsed_url.username if parsed_url.username else None,
-        'ROUNDCUBEMAIL_DB_PASSWORD': parsed_url.password if parsed_url.password else None,
-        'ROUNDCUBEMAIL_DB_NAME': parsed_url.path.strip('/') if parsed_url.path else None,
-    }
+REGISTRATION_FILE = (pathlib.Path('/share/matrix_appservices/') / socket.gethostname()).with_suffix('.yaml')
 
 
 if not OPTIONS_FILE.exists():
@@ -44,15 +24,6 @@ HA_options = json.loads(OPTIONS_FILE.read_text())
 heisenbridge_args = ['--config', str(REGISTRATION_FILE), '--listen-address', '0.0.0.0',
                      '--owner', HA_options['heisenbridge']['owner_mxid'], HA_options['heisenbridge']['synapse_url']]
 
-roundcube_env = HA_options['roundcube']
-if roundcube_env['database_url']:
-    roundcube_env.update(parse_dburl_for_roundcube(roundcube_env['database_url']))
-
-# Remove empty variables so that it lets the defaults happen rather than treating them as empty strings
-for k, v in list(roundcube_env.items()):
-    if not v:
-        roundcube_env.pop(k)
-
 if __name__ == "__main__":
     # Generate registration.yaml if it doesn't already exist
     if not REGISTRATION_FILE.exists():
@@ -61,11 +32,6 @@ if __name__ == "__main__":
         registration_yaml = yaml.safe_load(REGISTRATION_FILE.read_text())
         registration_yaml['url'] = HA_options['heisenbridge']['own_url']
         REGISTRATION_FILE.write_text(yaml.dump(registration_yaml))
-
-    if not DB_DIR.exists():
-        DB_DIR.mkdir()
-    if not CONFIG_DIR.exists():
-        CONFIG_DIR.mkdir()
 
     print('Starting Wireguard interface.', flush=True)
     WIREGUARD_CONF.write_text(
@@ -84,9 +50,7 @@ if __name__ == "__main__":
     subprocess.check_call(['ip', '-oneline', 'address'])
     print('Starting Heisenbridge with command:', ['heisenbridge', *heisenbridge_args], flush=True)
     heisenbridge = subprocess.Popen(['heisenbridge', *heisenbridge_args])
-    roundcube_args = ["/docker-entrypoint.sh", sys.argv[1] if len(sys.argv) >= 2 else "apache2-foreground", *sys.argv[2:]]
-    print('Starting Roundcube with command:', roundcube_args, flush=True)
-    roundcube = subprocess.Popen(roundcube_args, env={**os.environ, **roundcube_env})
+    roundcube = subprocess.Popen(['sleep', 'infinity'])
 
     crashed = False
     while crashed is False:
