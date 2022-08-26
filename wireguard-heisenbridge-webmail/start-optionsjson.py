2e1c8b02579e2594dcd988af4aa58659f22e16b0
Mike Abrahall <mike@abrahall.id.au>
2022-08-27 02:03:14 +1000

    Setup ttyd & Neomutt startup scripts

--- wireguard-heisenbridge-webmail/start-optionsjson.py
+++ wireguard-heisenbridge-webmail/start-optionsjson.py
@@ -13,6 +13,7 @@ import yaml
 OPTIONS_FILE = pathlib.Path('/data/options.json')
 WIREGUARD_CONF = pathlib.Path('/etc/wireguard/wg0.conf')
 REGISTRATION_FILE = (pathlib.Path('/share/matrix_appservices/') / socket.gethostname()).with_suffix('.yaml')
+MUTTRC_PATH = pathlib.Path('/tmp/Muttrc')
 
 
 if not OPTIONS_FILE.exists():
@@ -35,7 +36,7 @@ if __name__ == "__main__":
         print("Appservice registration yaml generated, go sort out registration before restarting this addon.",
               file=sys.stderr, flush=True)
     else:
-        print('Writing wg-quick config.', flush=True)
+        print('Writing wg-quick config', flush=True)
         WIREGUARD_CONF.write_text(
             '\n'.join((
                 "[Interface]",
@@ -47,14 +48,25 @@ if __name__ == "__main__":
                 "AllowedIPs = {joined_wireguard_allowed_IPs}",
             )).format(**HA_options, joined_wireguard_allowed_IPs=', '.join(HA_options['wireguard_allowed_IPs'])))
 
+        print('Writing muttrc config', flush=True)
+        if HA_options['muttrc']:
+            assert not MUTTRC_PATH.exists()
+            MUTTRC_PATH.write_text('\n'.join(HA_options['muttrc']))
+        # FIXME: Should I be programmatically determining this somehow? Mostly I just don't want it listening on the VPN
+        ttyd_nic = 'eth0'
+
         print('Starting Wireguard interface.', flush=True)
         subprocess.check_call(['wg-quick', 'up', 'wg0'])
-
         print('Got IP addresses;', flush=True)
         subprocess.check_call(['ip', '-oneline', 'address'])
+
         print('Starting Heisenbridge with command:', ['heisenbridge', *heisenbridge_args], flush=True)
         heisenbridge = subprocess.Popen(['heisenbridge', *heisenbridge_args])
-        roundcube = subprocess.Popen(['sleep', 'infinity'])
+
+        ttyd_mutt_args = ['runuser', '--user=nobody', '--', 'ttyd', '--port', '8099', '--interface', ttyd_nic,
+                          'neomutt', '-F', str(MUTTRC_PATH)]
+        print('Starting ttyd with command:', ttyd_mutt_args, flush=True)
+        ttyd_mutt = subprocess.Popen(ttyd_mutt_args)
 
         crashed = False
         while crashed is False:
@@ -63,8 +75,8 @@ if __name__ == "__main__":
             if pid == heisenbridge.pid:
                 crashed = True
                 print("Heisenbridge exited, killing Roundcube and exiting.", file=sys.stderr, flush=True)
-                roundcube.kill()
-            elif pid == roundcube.pid:
+                ttyd_mutt.kill()
+            elif pid == ttyd_mutt.pid:
                 crashed = True
-                print("Roundcube exited, killing Heisenbridge and exiting.", file=sys.stderr, flush=True)
+                print("ttyd exited, killing Heisenbridge and exiting.", file=sys.stderr, flush=True)
                 heisenbridge.kill()
