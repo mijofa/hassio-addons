78b7c227e3a39318a797cb2e4ca766f20de76440
Mike Abrahall <mike@abrahall.id.au>
2022-09-13 15:52:18 +1000

    Bringing roundcube back, giving up on Ingress until upstream fixes some bugs

--- wireguard-heisenbridge-webmail/start-optionsjson.py
+++ wireguard-heisenbridge-webmail/start-optionsjson.py
@@ -7,14 +7,33 @@ import pathlib
 import socket
 import subprocess
 import sys
+import urllib.parse
+import urllib.request
 
 import yaml
 
 OPTIONS_FILE = pathlib.Path('/data/options.json')
 WIREGUARD_CONF = pathlib.Path('/etc/wireguard/wg0.conf')
 REGISTRATION_FILE = (pathlib.Path('/share/matrix_appservices/') / socket.gethostname()).with_suffix('.yaml')
-MUTTRC_PATH = pathlib.Path('/tmp/Muttrc')
-SIGNATURE_PATH = pathlib.Path('/data/.signature')
+
+# I've symlinked some Roundcube directories into /data, but now Roundcube expects them to exist first
+# FIXME: Is it safe to just symlink them to /data directly?
+CONFIG_DIR = pathlib.Path('/data/config')
+DB_DIR = pathlib.Path('/data/db')
+
+
+def parse_dburl_for_roundcube(url: str):
+    """Parse URL string into a set of env vars for Roundcube."""
+    # FIXME: This could probably be a generator
+    parsed_url = urllib.parse.urlparse(url)
+    return {
+        'ROUNDCUBEMAIL_DB_TYPE': 'pgsql' if parsed_url.scheme == 'postgres' else parsed_url.scheme if parsed_url.scheme else None,
+        'ROUNDCUBEMAIL_DB_HOST': parsed_url.hostname if parsed_url.hostname else None,
+        'ROUNDCUBEMAIL_DB_PORT': parsed_url.port if parsed_url.port else None,
+        'ROUNDCUBEMAIL_DB_USER': parsed_url.username if parsed_url.username else None,
+        'ROUNDCUBEMAIL_DB_PASSWORD': parsed_url.password if parsed_url.password else None,
+        'ROUNDCUBEMAIL_DB_NAME': parsed_url.path.strip('/') if parsed_url.path else None,
+    }
 
 
 if not OPTIONS_FILE.exists():
@@ -25,6 +44,15 @@ HA_options = json.loads(OPTIONS_FILE.read_text())
 # FIXME: Don't listen on the VPN IP address.
 heisenbridge_args = ['--config', str(REGISTRATION_FILE), '--listen-address', '0.0.0.0',
                      '--owner', HA_options['heisenbridge_owner_mxid'], HA_options['heisenbridge_synapse_url']]
+
+roundcube_env = HA_options['roundcube']
+if roundcube_env['database_url']:
+    roundcube_env.update(parse_dburl_for_roundcube(roundcube_env['database_url']))
+
+# Remove empty variables so that it lets the defaults happen rather than treating them as empty strings
+for k, v in list(roundcube_env.items()):
+    if not v:
+        roundcube_env.pop(k)
 
 if __name__ == "__main__":
     # Generate registration.yaml if it doesn't already exist
@@ -49,14 +77,10 @@ if __name__ == "__main__":
                 "AllowedIPs = {joined_wireguard_allowed_IPs}",
             )).format(**HA_options, joined_wireguard_allowed_IPs=', '.join(HA_options['wireguard_allowed_IPs'])))
 
-        print('Writing muttrc config', flush=True)
-        if HA_options['muttrc']:
-            assert not MUTTRC_PATH.exists()
-            MUTTRC_PATH.write_text('\n'.join(HA_options['muttrc']))
-
-        print('Writing signature file', flush=True)
-        if HA_options['signature']:
-            SIGNATURE_PATH.write_text(HA_options['signature'])
+        if not DB_DIR.exists():
+            DB_DIR.mkdir()
+        if not CONFIG_DIR.exists():
+            CONFIG_DIR.mkdir()
 
         print('Starting Wireguard interface.', flush=True)
         subprocess.check_call(['wg-quick', 'up', 'wg0'])
@@ -66,14 +90,9 @@ if __name__ == "__main__":
         print('Starting Heisenbridge with command:', ['heisenbridge', *heisenbridge_args], flush=True)
         heisenbridge = subprocess.Popen(['heisenbridge', *heisenbridge_args])
 
-        # FIXME: Should I be programmatically determining this somehow? Mostly I just don't want it listening on the VPN
-        ttyd_nic = 'eth0'
-        # NOTE: 65534 = nobody/nogroup
-        ttyd_mutt_args = ['ttyd', '--port', '8099', '--interface', ttyd_nic,
-                          '--uid', '65534', '--gid', '65534', '--cwd', '/data',
-                          'neomutt', '-F', str(MUTTRC_PATH)]
-        print('Starting ttyd with command:', ttyd_mutt_args, flush=True)
-        ttyd_mutt = subprocess.Popen(ttyd_mutt_args, env={**os.environ, 'HOME': '/data'})
+        roundcube_args = ["/docker-entrypoint.sh", sys.argv[1] if len(sys.argv) >= 2 else "apache2-foreground", *sys.argv[2:]]
+        print('Starting Roundcube with env:', roundcube_env, flush=True)
+        roundcube = subprocess.Popen(roundcube_args, env={**os.environ, **roundcube_env})
 
         crashed = False
         while crashed is False:
@@ -82,8 +101,8 @@ if __name__ == "__main__":
             if pid == heisenbridge.pid:
                 crashed = True
                 print("Heisenbridge exited, killing Roundcube and exiting.", file=sys.stderr, flush=True)
-                ttyd_mutt.kill()
-            elif pid == ttyd_mutt.pid:
+                roundcube.kill()
+            elif pid == roundcube.pid:
                 crashed = True
                 print("ttyd exited, killing Heisenbridge and exiting.", file=sys.stderr, flush=True)
                 heisenbridge.kill()
