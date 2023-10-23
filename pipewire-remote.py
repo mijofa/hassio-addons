09064d8f334b0045ecce8876972aa71680c00faf
Mike Abrahall <mike@abrahall.id.au>
2023-10-23 17:36:45 +1100

    Get sink volume math sorted out

--- pipewire-remote.py
+++ pipewire-remote.py
@@ -13,6 +13,8 @@ import paho.mqtt.client
 #        https://www.home-assistant.io/integrations/mqtt/#mqtt-discovery
 #        Instead of relying on a single static topic.
 MQTT_TOPIC = 'update/input_boolean/music_inhibitor_mqtt'
+# FIXME: Handle this in Home Assistant
+PASSIVE_ROLES = ['Music']
 
 
 def read_pretty_json_list(fp):
@@ -38,9 +40,45 @@ def pipewire_events():
             yield ev
 
 
+def _vol_to_percentage(vol):
+    """Return the given volume as a percentage."""
+    # I don't understand what the input number actually is,
+    # or the math behind fixing this.
+    # But I found someone else doing this with awk:
+    # https://gist.github.com/venam/bd453b4fd673ff8abb9323e69f182045
+    # and doing the same in Python has worked with everything I've thrown at it
+    return vol**(1 / 3)
+
+
+def get_sink_state(sink_info):
+    """
+    Get the current volume (as a percentage) from the given sink info.
+
+    Returns:
+    * True if muted, False if note
+    * The average volume across all channels
+    * The name & volume of each channel
+    """
+    print(sink_info['props']['node.name'],
+          sink_info['props']['node.description'])
+    channels = {}
+    for num, name in enumerate(sink_info['params']['Props'][0]['channelMap']):
+        if sink_info['params']['Props'][0]['softVolumes'][num] != 1 and \
+           sink_info['params']['Props'][0]['softVolumes'][num] != sink_info['params']['Props'][0]['channelVolumes'][num]:
+            raise NotImplementedError("Not seen in testing")
+        else:
+            channels[name] = _vol_to_percentage(sink_info['params']['Props'][0]['channelVolumes'][num])
+
+    # In testing I never saw 'softMute' be True,
+    # so I'm just kind of assuming how this works.
+    muted = sink_info['params']['Props'][0]['mute'] or sink_info['params']['Props'][0]['softMute']
+
+    return (muted, sum(channels.values()) / len(channels), channels)
+
+
 # Since upstream **still** hasn't fixed this 3yr old bug
 # https://github.com/eclipse/paho.mqtt.python/issues/493
-# I've copy/pasted upstream's connect_srv function myself.
+# I've copy/pasted upstream's connect_srv function to fix it internally.
 # I've also pushed my own pull request for them
 # https://github.com/eclipse/paho.mqtt.python/pull/759
 # but I get the feeling it's going to be ignored
@@ -84,16 +122,19 @@ def connect_srv(mqtt_client, domain=None, *args, **kwargs):
 
 
 mqtt_client = paho.mqtt.client.Client()
+# NOTE: The will must be set before connecting.
+mqtt_client.will_set(topic=MQTT_TOPIC, payload='unavailable', retain=True)
+
 # FIXME: Try anonymous, and fallback on guest:guest when that fails
 mqtt_client.username_pw_set(username='guest', password='guest')
 connect_srv(mqtt_client)
 mqtt_client.loop_start()
-# FIXME: This doesn't seem to be working
-mqtt_client.will_set(topic=MQTT_TOPIC, payload='unavailable', retain=True)
 
 previous_payload = None
 playback_streams = {}
 output_sinks = {}
+# FIXME: Can I make this wait until after the first load of events is handled before first updating the payload?
+#        Because that first set of dumped events tends to bounce the inhibitor on/off annoyingly.
 for ev in pipewire_events():
     if 'type' not in ev and ev.get('info') is None:
         # This is a node being removed, but we don't know what type of node
@@ -112,7 +153,7 @@ for ev in pipewire_events():
 
                 playback_streams[ev['id']] = (ev['info']['state'],
                                               ev['info']['props'])
-                print(ev['info']['state'],
+                print(ev['info']['state'], 'playback stream',
                       ev['info']['props']['application.name'],
                       ev['info']['props'].get('media.role'))
             case 'Audio/Sink':
@@ -121,14 +162,14 @@ for ev in pipewire_events():
                     # Don't care about changes outside of 'params'
                     continue
 
-                print(ev['info']['props']['node.name'],
-                      ev['info']['props']['node.description'])
-                print(ev['info']['params']['Props'][0]['mute'],
-                      # FIXME: What math do I need for a single volume number?
-                      ev['info']['params']['Props'][0]['volume'],
-                      ev['info']['params']['Props'][0]['channelVolumes'])
+                output_sinks[ev['id']] = (ev['info']['state'], get_sink_state(ev['info']))
+                print(ev['info']['state'], 'output sink',
+                      ev['info']['props']['node.description'],
+                      ev['info']['props']['node.name'],
+                      get_sink_state(ev['info']))
 
-    if any((state == 'running' for state, props in playback_streams.values())):
+    if any((props.get('media.role') not in PASSIVE_ROLES for state, props in playback_streams.values()
+            if state == 'running' for state, props in playback_streams.values())):
         payload = 'on'
     else:
         payload = 'off'
