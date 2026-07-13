5c8406f2e25ae8962f2146acfd8eb9f9035d6c75
Mike Abrahall <mike@abrahall.id.au>
2026-07-13 14:29:14 +1000

    fixup! Dirty hack to avoid Cloudflare pissing me off

--- pipewire-mqtt.py
+++ pipewire-mqtt.py
@@ -229,7 +229,7 @@ for ev in pipewire_events():
 
                 # Fuck off Cloudflare!!
                 proc_bin_name = ev['info']['props'].get('application.process.binary', '')
-                if (ev['info']['props'].get('media.name', False) == "Checking your Browser…" and
+                if (ev['info']['props'].get('media.name', False) in ("Checking your Browser…", "Just a moment...") and
                         ('firefox' in proc_bin_name or 'chrome' in proc_bin_name or 'chromium' in proc_bin_name)):
                     # If I leave a cloudflare tab open, it just keeps spamming new "Checking your Browser…" players constantly.
                     # It doesn't actually make any sound, and it's annoying as hell.
